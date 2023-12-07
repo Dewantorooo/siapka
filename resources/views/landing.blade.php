80f426a8eace6b363861701be423b29d04b05647
Reza Apriansyah Sune <sunereza@gmail.com>
2023-12-07 23:10:19 +0700

    Menghapus Comment

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -169,34 +169,3 @@
 </body>
 
 </html>
-{{-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Landing Page</title>
-
-  <link href="/css/bootstrap.min.css" rel="stylesheet">
-</head>
-
-<body>
-  @auth
-    <h1>Anda sudah login</h1>
-  @else
-    @if (session()->has('success'))
-      <div class="alert alert-success">{{ session('success') }}</div>
-@endif
-@if (session()->has('error'))
-<div class="alert alert-danger">{{ session('error') }}</div>
-@endif
-
-<a href="/register/peserta_didik">Daftar</a>
-<a href="/login">Login</a>
-@endauth
-
-<script src="/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html> --}}
